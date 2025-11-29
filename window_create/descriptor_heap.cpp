9d54bcc2058e4efc156a54b46eafa62f60f3793a
M <ktc24a1a0065@edu.kyoto-tech.ac.jp>
2025-11-29 18:04:13 +0900

    desciptorheapの作成途中

--- window_create/descriptor_heap.cpp
+++ window_create/descriptor_heap.cpp
@@ -1,7 +1,26 @@
+#include "descriptor_heap.h"
+#include <cassert>
 
+//-------------------------------------------------------------------------
+//デストラクタ
+Descriptorheap::~Descriptorheap() {
+	if (heap_) {
+		heap_->Release();
+		heap_ = nullptr;
+	}
+}
 
+//-------------------------------------------------------------------------
+//ディスクリプタヒープを生成する
+[[nodiscard]] bool Descriptorheap::create(const Dx12& dx12, D3D12_DESCRIPTOR_HEAP_TYPE type, UINT numDescriptors, bool shaderVisible) noexcept {
+	//ヒープの設定
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+	heapDesc.Type                       = type;
+	heapDesc.NumDescriptors             = numDescriptors;
+	heapDesc.Flags                      = shaderVisible ? D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE : D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
 
+	type_ = type; //ヒープのタイプを保存
 
-
-
-
+	// ディスクリプタヒープの生成
+	//HRESULT hr = dx12.get()->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&heap_));
+}
--- window_create/descriptor_heap.h
+++ window_create/descriptor_heap.h
@@ -1,2 +1,31 @@
 #pragma once
 
+#include "Dx12.h"
+
+class Descriptorheap final {
+public:
+	//-------------------------------------------------------------
+	//コンストラクタ
+	Descriptorheap() = default;
+
+	//-------------------------------------------------------------
+	//デストラクタ
+	~Descriptorheap();
+
+	//-------------------------------------------------------------
+	//ディスクリプタヒープを生成する
+	[[nodiscard]] bool create(const Dx12& dx12,D3D12_DESCRIPTOR_HEAP_TYPE type,UINT numDescriptors,bool shaderVisible = false) noexcept;
+
+	//-------------------------------------------------------------
+	//ディスクリプタヒープを取得する
+	[[nodiscard]] ID3D12DescriptorHeap* get() const noexcept;
+
+	//-------------------------------------------------------------
+	//ディスクリプタヒープのタイプを取得する
+	[[nodiscard]] D3D12_DESCRIPTOR_HEAP_TYPE getType() const noexcept;
+
+private:
+	ID3D12DescriptorHeap*        heap_{}; //ディスクリプタヒープ
+	D3D12_DESCRIPTOR_HEAP_TYPE   type_{}; //ヒープのタイプ
+};
+
